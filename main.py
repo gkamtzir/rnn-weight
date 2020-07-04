a1526e9287a349a155fa861d69594498c1fe4eca
George Kamtziridis <georgekam96@gmail.com>
2020-07-04 17:51:39 +0300

    Restructured the main file.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import plac
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -32,7 +33,7 @@ def load_data():
     testing_set = dataset_test.iloc[:, 0].values.reshape(-1, 1)
     return dataset_train, training_set, dataset_test, testing_set
 
-def train_rnn(X_train, y_train):
+def train_rnn(X_train, y_train, epochs, output_weights):
     # Creating RNN.
     regressor = Sequential()
     
@@ -59,66 +60,73 @@ def train_rnn(X_train, y_train):
     regressor.compile(optimizer = 'adam', loss = 'mean_squared_error')
     
     # Fitting the RNN to the Training set.
-    regressor.fit(X_train, y_train, epochs = 100, batch_size = 32)
+    regressor.fit(X_train, y_train, epochs = epochs, batch_size = 32)
     
     # Saving the model.
-    regressor.save('weights_model.h5')
+    regressor.save(f"{output_weights}.h5")
     
     return regressor
 
-# Applying scaling.
-from sklearn.preprocessing import MinMaxScaler
-scaler = MinMaxScaler(feature_range = (0, 1))
-
-train_model = False
-
-dataset_train, training_set, dataset_test, testing_set = load_data()
-
-# Preparing the training set.
-window = 4
-X_train = []
-y_train = []
-if train_model:
-    training_set = scaler.fit_transform(training_set)
-    for i in range(window, training_set.shape[0]):
-        X_train.append(training_set[i - window:i, 0])
-        y_train.append(training_set[i, 0])
-    X_train, y_train = np.array(X_train), np.array(y_train)
-    X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 1))
+@plac.annotations(
+    model=("Model name. Defaults to blank 'el' model.", "option", "m", str),
+    output_weights=("Weights file name", "option", "o", str),
+    window=("LSTM window", "option", "w", int),
+    epochs=("Number of training epochs", "option", "e", int),
+)
+def main(model = None, output_weights = "weights_model", window = 4, epochs = 100):
+    # Applying scaling.
+    from sklearn.preprocessing import MinMaxScaler
+    scaler = MinMaxScaler(feature_range = (0, 1))
     
-    # Training the RNN.
-    regressor = train_rnn(X_train, y_train)
+    dataset_train, training_set, dataset_test, testing_set = load_data()
     
-    # Testing.
-    dataset_total = pd.concat((dataset_train[0], dataset_test[0]), axis = 0)
-    inputs = dataset_total[len(dataset_total) - len(dataset_test) - window:].values
-    inputs = inputs.reshape(-1,1)
-    inputs = scaler.transform(inputs)
-    X_test = []
-    for i in range(window, inputs.shape[0]):
-        X_test.append(inputs[i-window:i, 0])
+    # Preparing the training set.
+    X_train = []
+    y_train = []
+    if model is None:
+        training_set = scaler.fit_transform(training_set)
+        for i in range(window, training_set.shape[0]):
+            X_train.append(training_set[i - window:i, 0])
+            y_train.append(training_set[i, 0])
+        X_train, y_train = np.array(X_train), np.array(y_train)
+        X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 1))
         
-    predicted_weight = predict(X_test, regressor, scaler)
-    
-    visualize_results(testing_set, predicted_weight)
-else:
-    # Loading model.
-    regressor = load_model('weights_model.h5')
-    dataset_total = pd.concat((dataset_train[0], dataset_test[0]), axis = 0)
-    inputs = dataset_total[:].values
-    inputs = inputs.reshape(-1,1)
-    inputs = scaler.fit_transform(inputs)
-    for i in range(window, inputs.shape[0]):
-        X_train.append(inputs[i - window:i, 0])
-        y_train.append(inputs[i, 0])
-    X_train, y_train = np.array(X_train), np.array(y_train)
-    X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 1))
-    
-    # Testing.
-    inputs = dataset_total[len(dataset_total) - window:].values
-    inputs = inputs.reshape(-1,1)
-    inputs = scaler.transform(inputs)
-    X_test = [inputs[:, 0]]
-    
-    predicted_weight = predict(X_test, regressor, scaler)
-    print(f"Tomorrow your weight will be: {predicted_weight[0][0]}")
+        # Training the RNN.
+        regressor = train_rnn(X_train, y_train, epochs, output_weights)
+        
+        # Testing.
+        dataset_total = pd.concat((dataset_train[0], dataset_test[0]), axis = 0)
+        inputs = dataset_total[len(dataset_total) - len(dataset_test) - window:].values
+        inputs = inputs.reshape(-1,1)
+        inputs = scaler.transform(inputs)
+        X_test = []
+        for i in range(window, inputs.shape[0]):
+            X_test.append(inputs[i-window:i, 0])
+            
+        predicted_weight = predict(X_test, regressor, scaler)
+        
+        visualize_results(testing_set, predicted_weight)
+    else:
+        # Loading model.
+        regressor = load_model(f"{model}.h5")
+        dataset_total = pd.concat((dataset_train[0], dataset_test[0]), axis = 0)
+        inputs = dataset_total[:].values
+        inputs = inputs.reshape(-1,1)
+        inputs = scaler.fit_transform(inputs)
+        for i in range(window, inputs.shape[0]):
+            X_train.append(inputs[i - window:i, 0])
+            y_train.append(inputs[i, 0])
+        X_train, y_train = np.array(X_train), np.array(y_train)
+        X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 1))
+        
+        # Testing.
+        inputs = dataset_total[len(dataset_total) - window:].values
+        inputs = inputs.reshape(-1,1)
+        inputs = scaler.transform(inputs)
+        X_test = [inputs[:, 0]]
+        
+        predicted_weight = predict(X_test, regressor, scaler)
+        print(f"Tomorrow your weight will be: {predicted_weight[0][0]}")
+        
+if __name__ == "__main__":
+    plac.call(main)
